2469784a5da54a71c59207cd4e94757ca711d100
cdecaprio <cdecaprio@cendrassos.net>
2025-04-30 02:26:58 +0200

    no message

--- resources/js/Pages/Musiclist.vue
+++ resources/js/Pages/Musiclist.vue
@@ -9,24 +9,24 @@ const props = defineProps({
 <template>
     <AuthenticatedLayout>
         <div class="screen-h-screen">
-        <div class="container mx-auto mt-4">
             <div class="container mx-auto mt-4">
-                <h1>Music List</h1>
+                <div class="container mx-auto mt-4">
+                    <h1>Music List</h1>
+                </div>
+
             </div>
-            
-        </div>
-        
-        <div class="container mx-auto mt-4 mb-4">
-        <div v-for="music in props.music" :key="music.id">
+
             <div class="container mx-auto mt-4 mb-4">
-                <h1>{{ music.title }}</h1>
-                <img :src="`/storage/${music.image}`" alt="music image" class="w-10 h-10 rounded-md">
-                <audio :src="`/storage/${music.song}`" controls></audio>
-                <p>{{ music.artist }}</p>
+                <div v-for="music in props.music" :key="music.id">
+                    <div class="container mx-auto mt-4 mb-4">
+                        <h1>{{ music.title }}</h1>
+                        <img :src="`/storage/${music.image}`" alt="music image" class="w-10 h-10 rounded-md">
+                        <audio :src="`/storage/${music.song}`" controls></audio>
+                        <p>{{ music.artist }}</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
 
     </AuthenticatedLayout>
 </template>
